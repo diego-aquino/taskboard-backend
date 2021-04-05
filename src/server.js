832c7345a57a1bbe0a83cd32b056bff4485bcf3f
Diego Aquino <diego.cruz001@gmail.com>
2021-04-05 08:21:39 -0300

    Set up integration tests agent

--- src/server.js
+++ src/server.js
@@ -1,19 +1,11 @@
-import express from 'express';
-import cors from 'cors';
 import 'dotenv/config';
 
-import routes from '~/routes';
+import app from '~/app';
 import config from '~/config';
 import database from '~/database';
 
-const server = express();
-server.use(express.json());
-server.use(cors());
-
 database.connect();
 
-server.use(routes);
-
-server.listen(config.serverPort, () => {
+app.listen(config.serverPort, () => {
   console.log(`[server] Server is listening on port ${config.serverPort}...`);
 });
