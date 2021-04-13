b1d4a9b66af1875afc364c2ae4ca29c752f87627
Diego Aquino <diego.cruz001@gmail.com>
2021-04-13 20:47:04 -0300

    Apply custom styles to improve docs UI

---
+++ src/config/docs.js
@@ -0,0 +1,44 @@
+const docsConfig = {
+  customSwaggerCss: `
+    @import url(
+      'https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap'
+    );
+
+    .responses-wrapper > .responses-inner td.response-col_description {
+      padding-top: 0;
+    }
+
+    .responses-wrapper > .responses-inner td.response-col_status,
+    .responses-wrapper > .responses-inner td.response-col_links {
+      padding-top: 1rem;
+    }
+
+    .responses-wrapper > .responses-inner td.response-col_status {
+      font-weight: bold;
+      font-size: 1rem;
+      min-width: 5em;
+    }
+
+    .responses-wrapper > .responses-inner td.response-col_description pre {
+      min-height: 0;
+      padding: 1rem !important;
+    }
+
+    .responses-wrapper > .responses-inner td.response-col_description pre
+      code {
+      font-family: 'JetBrains Mono', monospace;
+    }
+
+    .responses-wrapper > .responses-inner td.response-col_description
+      > div:last-of-type {
+      padding-bottom: 1.5rem;
+    }
+
+    .responses-wrapper > .responses-inner td.response-col_description
+      > div.response-col_description__inner > div > p {
+      margin: 1rem auto 0;
+    }
+  `,
+};
+
+export default docsConfig;
--- src/routes/docsRoutes.js
+++ src/routes/docsRoutes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import swagger from 'swagger-ui-express';
 
+import config from '~/config';
 import docs from '~docs';
 
 const docsRoutes = Router();
 
-docsRoutes.use('/docs', swagger.serve, swagger.setup(docs));
+docsRoutes.use('/docs', swagger.serve);
+docsRoutes.get(
+  '/docs',
+  swagger.setup(docs, { customCss: config.docs.customSwaggerCss }),
+);
 
 export default docsRoutes;
